6e702e01f48374b5073e807a75d0ca894d970483
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-04-10 16:40:18 -0500

    Initial server setup & api
    api, types, bson, json

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+
+# CMD
+- ./bin/api --listenAddr :7000
+
+# 3rd Party Packages
+- Fiber Web Framework
---
+++ types/user.go
@@ -0,0 +1,8 @@
+package types
+
+// Same concept as model in MVC
+type User struct {
+	ID        string `bson:"_id" json:"id,omitempty"`
+	FirstName string `bson:"firstName" json:"firstName"`
+	LastName  string `bson:"lastName" json:"lastName"`
+}
