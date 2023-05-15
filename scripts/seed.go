e5bf5eec42312abe923523e97d6983a07f292eca
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-05-15 16:52:02 -0500

    Creating test fixtures 1

--- scripts/seed.go
+++ scripts/seed.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/jinheehanaaa/hotel-reservation/api"
 	"github.com/jinheehanaaa/hotel-reservation/db"
+	"github.com/jinheehanaaa/hotel-reservation/db/fixtures"
 	"github.com/jinheehanaaa/hotel-reservation/types"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -86,16 +87,37 @@ func seedHotel(name string, location string, rating int) *types.Hotel {
 }
 
 func main() {
+	var err error
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := client.Database(db.DBNAME).Drop(ctx); err != nil {
+		log.Fatal(err)
+	}
+	hotelStore := db.NewMongoHotelStore(client)
+	store := &db.Store{
+		User:    db.NewMongoUserStore(client),
+		Booking: db.NewMongoBookingStore(client),
+		Room:    db.NewMongoRoomStore(client, hotelStore),
+		Hotel:   hotelStore,
+	}
 
-	jinhee := seedUser(false, "Jinhee", "Han", "jinheehanaaa@gmail.com", "supersecurepassword")
-	seedUser(true, "admin", "admin", "admin@admin.com", "adminpassword123")
-	seedHotel("Bellucia", "France", 3)
-	seedHotel("The cozy hotel", "The Nederlands", 4)
-	hotel := seedHotel("Dont die in your sleep", "London", 1)
-	seedRoom("small", true, 89.99, hotel.ID)
-	seedRoom("medium", true, 189.99, hotel.ID)
-	room := seedRoom("large", false, 289.99, hotel.ID)
-	seedBooking(jinhee.ID, room.ID, time.Now(), time.Now().AddDate(0, 0, 2))
+	user := fixtures.AddUser(store, "james", "foo", false)
+	hotel := fixtures.AddHotel(store, "some  hotel", "bermuda", 5, nil)
+	room := fixtures.AddRoom(store, "large", true, 88.44, hotel.ID)
+	booking := fixtures.AddBooking(store, user.ID, room.ID, time.Now(), time.Now().AddDate(0, 0, 5))
+	fmt.Println(booking)
+	return
+	// jinhee := seedUser(false, "Jinhee", "Han", "jinheehanaaa@gmail.com", "supersecurepassword")
+	// seedUser(true, "admin", "admin", "admin@admin.com", "adminpassword123")
+	// seedHotel("Bellucia", "France", 3)
+	// seedHotel("The cozy hotel", "The Nederlands", 4)
+	// hotel := seedHotel("Dont die in your sleep", "London", 1)
+	// seedRoom("small", true, 89.99, hotel.ID)
+	// seedRoom("medium", true, 189.99, hotel.ID)
+	// room := seedRoom("large", false, 289.99, hotel.ID)
+	// seedBooking(jinhee.ID, room.ID, time.Now(), time.Now().AddDate(0, 0, 2))
 }
 
 func init() {
